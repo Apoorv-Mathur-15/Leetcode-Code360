f33a7a6754e4824d277ca5e24d7e726c5d77c56f
Apoorv-Mathur-15 <mathur.apoorv367@gmail.com>
2025-02-21 11:10:08 +0530

    21st Feb 2025

---
+++ src/CodingNinja/SubarrayXorK.java
@@ -0,0 +1,34 @@
+package src.CodingNinja;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SubarrayXorK {
+    public static int subarraysWithSumK(int []a, int b) {
+        int n = a.length;
+        int xr = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(xr,1);
+        int count = 0;
+        for(int i : a){
+            xr = xr ^ i;
+            int x  = xr ^ b;
+            if(map.containsKey(x))
+                count += map.get(x);
+            if(map.containsKey(xr))
+                map.put(xr, map.get(xr) + 1);
+            else
+                map.put(xr, 1);
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {1,2,3,2};
+        int[] a1 = {1,2,3,3};
+        int[] a2 = {1,3,3,3,5};
+        System.out.println(subarraysWithSumK(a, 2));
+        System.out.println(subarraysWithSumK(a1, 3));
+        System.out.println(subarraysWithSumK(a2, 6));
+    }
+}
