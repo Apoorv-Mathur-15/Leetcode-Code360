bf82103c280a84ef3ec48c555e8d2fbbd36e700d
Apoorv-Mathur-15 <mathur.apoorv367@gmail.com>
2025-02-25 10:59:19 +0530

    25th Feb 2025

--- src/CodingNinja/TotalSalary.java
+++ src/CodingNinja/TotalSalary.java
@@ -8,13 +8,27 @@ public class TotalSalary {
         System.out.println("Enter the basic salary and the grade: ");
         int basic = scanner.nextInt();
         String s = scanner.next();
-        int totalSalary = (int) Math.round(basic + basic*0.2 + basic*0.5 -basic*0.11);
-        if(s == "A")
+        double hra = basic*0.2;
+        double da = basic*0.5;
+        double pf = basic*0.11;
+        int totalSalary = 0;
+        if(s.equals("A")){
             totalSalary += 1700;
-        else if (s == "B")
+            //System.out.println("A");
+        }
+        else if (s.equals("B")){
             totalSalary += 1500;
-        else
+            //System.out.println("B");
+        }
+        else{
             totalSalary += 1300;
-        System.out.println(totalSalary);
+            //System.out.println("Else");
+        }
+        totalSalary += (int) Math.round(basic + hra + da -pf);
+        System.out.println("basic "+basic);
+        System.out.println("HRA "+hra);
+        System.out.println("DA "+da);
+        System.out.println("PF "+pf);
+        System.out.println("totalSalary "+totalSalary);
     }
 }
