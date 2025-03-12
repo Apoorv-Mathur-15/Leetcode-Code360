2ae3c741d773fa15451de0b7d867ebc251819219
Apoorv-Mathur-15 <mathur.apoorv367@gmail.com>
2025-03-12 11:47:03 +0530

    12th Mar 2025

---
+++ src/CodingNinja/MajorityElementII.java
@@ -0,0 +1,48 @@
+package src.CodingNinja;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MajorityElementII {
+    public static ArrayList<Integer> majorityElementII(ArrayList<Integer> arr){
+        ArrayList<Integer> result = new ArrayList<>();
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        for (int i : arr) {
+            if (hashMap.containsKey(i))
+                hashMap.put(i, hashMap.get(i) + 1);
+            else
+                hashMap.put(i, 1);
+        }
+        int floor = arr.size()/3;
+        for(Map.Entry<Integer, Integer> map : hashMap.entrySet()){
+            if(map.getValue() > floor)
+                result.add(map.getKey());
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> arr1 = new ArrayList<>();
+        ArrayList<Integer> arr2 = new ArrayList<>();
+        arr1.add(3);
+        arr1.add(2);
+        arr1.add(2);
+        arr1.add(1);
+        arr1.add(5);
+        arr1.add(2);
+        arr1.add(3);
+        arr2.add(7);
+        arr2.add(4);
+        arr2.add(4);
+        arr2.add(9);
+        arr2.add(7);
+        ArrayList<Integer> result1 = majorityElementII(arr1);
+        ArrayList<Integer> result2 = majorityElementII(arr2);
+        for(int i : result1)
+            System.out.println(i);
+        System.out.println("-------------------------");
+        for(int i : result2)
+            System.out.println(i);
+    }
+}
