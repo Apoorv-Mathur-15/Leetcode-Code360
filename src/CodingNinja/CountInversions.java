d2756e797b375c024d1b55d79c875a9a8ca2ea1a
Apoorv-Mathur-15 <mathur.apoorv367@gmail.com>
2025-03-12 11:38:24 +0530

    12th Mar 2025

---
+++ src/CodingNinja/CountInversions.java
@@ -0,0 +1,19 @@
+package src.CodingNinja;
+
+public class CountInversions {
+    public static long getInversions(long arr[], int n){
+        long inversions = 0;
+        for(int i=0; i<n-1; i++){
+            for(int j=i+1; j<n; j++){
+                if(arr[i] > arr[j])
+                    inversions++;
+            }
+        }
+        return inversions;
+    }
+
+    public static void main(String[] args) {
+        long[] arr = {2,5,1,3,4};
+        System.out.println(getInversions(arr, arr.length));
+    }
+}
