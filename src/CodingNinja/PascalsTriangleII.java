e39dd44d960b419ad655aeb2a923077b50748e95
Apoorv-Mathur-15 <mathur.apoorv367@gmail.com>
2025-03-15 12:38:01 +0530

    15th Mar 2025

---
+++ src/CodingNinja/PascalsTriangleII.java
@@ -0,0 +1,37 @@
+package src.CodingNinja;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PascalsTriangleII {
+    public static int nCr(int n, int r) {
+        long res = 1;
+        for (int i = 0; i < r; i++) {
+            res = res * (n - i);
+            res = res / (i + 1);
+        }
+        return (int) res;
+    }
+    public static ArrayList<ArrayList<Long>> printPascal(int n) {
+        ArrayList<ArrayList<Long>> result = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            ArrayList<Long> list = new ArrayList<>();
+            for (int j = 0; j <= i; j++) {
+                int number = nCr(i, j);
+                list.add((long) number);
+            }
+            result.add(list);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<ArrayList<Long>> ans = printPascal(1);
+        for(ArrayList<Long> a1 : ans ){
+            for (Long n : a1)
+                System.out.print(n+" ");
+            System.out.println();
+        }
+    }
+}
