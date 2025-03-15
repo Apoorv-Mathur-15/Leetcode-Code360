c93d53cf9738627c4e0201184a335dd7eb955ae2
Apoorv-Mathur-15 <mathur.apoorv367@gmail.com>
2025-03-15 12:00:41 +0530

    15th Mar 2025

--- src/CodingNinja/Search2DMatrix.java
+++ src/CodingNinja/Search2DMatrix.java
@@ -5,8 +5,8 @@ import java.util.ArrayList;
 public class Search2DMatrix {
     public static boolean searchMatrix(ArrayList<ArrayList<Integer>> mat, int target){
         for(ArrayList<Integer> matrix : mat){
-            for(int i=0; i<matrix.size(); i++){
-                if(matrix.get(i) == target)
+            for (Integer integer : matrix) {
+                if (integer == target)
                     return true;
             }
         }
