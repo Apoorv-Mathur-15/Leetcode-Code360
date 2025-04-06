fa6482ec850171c903173e11da454ffc5ecc34b7
Apoorv-Mathur-15 <mathur.apoorv367@gmail.com>
2025-04-06 22:31:15 +0530

    6th Apr 2025

--- src/CodingNinja/GetSingleElement.java
+++ src/CodingNinja/GetSingleElement.java
@@ -1,4 +1,4 @@
-package CodingNinja;
+package src.CodingNinja;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -7,12 +7,8 @@ public class GetSingleElement {
     public static int getSingleElement(int[] arr) {
         Map<Integer, Integer> map = new HashMap<>();
         int n = arr.length;
-        for (int i = 0; i < n; i++) {
-            if (map.containsKey(arr[i]))
-                map.put(arr[i], map.get(arr[i]) + 1);
-            else
-                map.put(arr[i], 1);
-        }
+        for(int i : arr)
+            map.put(i, map.getOrDefault(i, 0) + 1);
         for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
             if (entry.getValue().equals(1))
                 return entry.getKey();
