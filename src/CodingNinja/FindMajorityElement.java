e5d16bfdd2abf8a9ad99b6aa9be59a12fb5bcb6d
Apoorv-Mathur-15 <mathur.apoorv367@gmail.com>
2025-04-06 18:46:43 +0530

    6th Apr 2025

--- src/CodingNinja/FindMajorityElement.java
+++ src/CodingNinja/FindMajorityElement.java
@@ -7,10 +7,7 @@ public class FindMajorityElement {
     public static int findMajority(int[] arr, int n) {
         HashMap<Integer, Integer> hashMap = new HashMap<>();
         for (int i : arr) {
-            if (hashMap.containsKey(i))
-                hashMap.put(i, hashMap.get(i) + 1);
-            else
-                hashMap.put(i, 1);
+            hashMap.put(i, hashMap.getOrDefault(i, 0) + 1);
         }
         int floor = n/2;
         for(Map.Entry<Integer, Integer> map : hashMap.entrySet()){
