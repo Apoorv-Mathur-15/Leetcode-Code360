80b74e763d2355a72701ea8d84c87579fb24b070
Apoorv-Mathur-15 <mathur.apoorv367@gmail.com>
2025-04-19 18:30:13 +0530

    19th Apr 2025

--- src/CodingNinja/MaxDifference.java
+++ src/CodingNinja/MaxDifference.java
@@ -1,4 +1,25 @@
 package src.CodingNinja;
 
 public class MaxDifference {
+
+    public static int maximumDifference(String s) {
+        int maxDiff = Integer.MIN_VALUE;
+        int currentSum = 0;
+        boolean hasZero = false;
+
+        for (int i = 0; i < s.length(); i++) {
+            // '0' contributes +1, '1' contributes -1
+            int value = (s.charAt(i) == '0') ? 1 : -1;
+            if (s.charAt(i) == '0') hasZero = true;
+
+            currentSum += value;
+            maxDiff = Math.max(maxDiff, currentSum);
+
+            // Reset currentSum if it goes below 0
+            if (currentSum < 0) currentSum = 0;
+        }
+
+        // If there is no '0' in the string, we cannot have a positive difference
+        return hasZero ? maxDiff : -1;
+    }
 }
