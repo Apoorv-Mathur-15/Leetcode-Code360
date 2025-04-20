95e9ba5f112a84876c34e3a6f5a06c76156df7d6
Apoorv-Mathur-15 <mathur.apoorv367@gmail.com>
2025-04-20 09:50:32 +0530

    20th Apr 2025

---
+++ src/CodingNinja/ToLowerCase.java
@@ -0,0 +1,11 @@
+package src.CodingNinja;
+
+public class ToLowerCase {
+    public static String toLowerCase(String s) {
+        return s.toLowerCase();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(toLowerCase("AbcdEfgh"));
+    }
+}
