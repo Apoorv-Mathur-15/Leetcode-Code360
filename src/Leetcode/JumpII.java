2b1b70869b506e9122e5347cc42af79f0d8480e8
Apoorv-Mathur-15 <mathur.apoorv367@gmail.com>
2025-06-01 09:26:42 +0530

    1st Jun 2025

--- src/Leetcode/JumpII.java
+++ src/Leetcode/JumpII.java
@@ -1,4 +1,23 @@
 package src.Leetcode;
 
 public class JumpII {
+    public static int jump(int[] nums){
+        int maxReach = 0, steps = 0, end = 0;
+
+        for(int i=0; i<nums.length-1; i++) {
+            maxReach = Math.max(maxReach, i + nums[i]);
+            if(i == end) {
+                end = maxReach;
+                steps++;
+            }
+        }
+        return steps;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2,3,1,1,4};
+        int[] nums1 = {2,3,0,1,4};
+        System.out.println(jump(nums));
+        System.out.println(jump(nums1));
+    }
 }
