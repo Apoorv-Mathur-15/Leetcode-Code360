dbe54cc4f17adaf8d3390976428ccda5a3faa2ba
Apoorv-Mathur-15 <mathur.apoorv367@gmail.com>
2025-08-30 18:50:57 +0530

    30th Aug 2025

--- src/Leetcode/CombinationSumII.java
+++ src/Leetcode/CombinationSumII.java
@@ -2,25 +2,43 @@ package src.Leetcode;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 public class CombinationSumII {
-    public static List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> result = new ArrayList<>();
+    public static List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> sums = new ArrayList<>();
+        // optimize
         Arrays.sort(candidates);
-        backtrace(result, new ArrayList<>(), candidates, target, 0);
-        return result;
+        combinationSum(candidates, target, 0, sums, new LinkedList<>());
+        return sums;
     }
 
-    private static void backtrace(List<List<Integer>> result, List<Integer> combination, int[] candidate, int target, int start) {
-        if(target == 0){
-            result.add(new ArrayList<>(combination));
+    private static void combinationSum( int[] candidates, int target, int start, List<List<Integer>> sums, LinkedList<Integer> sum) {
+        if (target == 0) {
+            // make a deep copy of the current combination
+            sums.add(new ArrayList<>(sum));
             return;
         }
-        for(int i=start; i < candidate.length && candidate[i] <= target; i++) {
-            combination.add(candidate[i]);
-            backtrace(result, combination, candidate, target - candidate[i], i);
-            combination.remove(combination.size() - 1);
+        for (int i = start; i < candidates.length && target >= candidates[i]; i++) {
+            // If candidate[i] equals candidate[i-1], then solutions for i is subset of
+            // solution of i-1
+            if (i == start || (i > start && candidates[i] != candidates[i - 1])) {
+                sum.addLast(candidates[i]);
+                // call on 'i+1' (not i) to avoid duplicate usage of same element
+                combinationSum(candidates, target - candidates[i], i + 1, sums, sum);
+                sum.removeLast();
+            }
+        }
+    }
+
+    public static void main() {
+        int[] candidates = new int[]{10,1,2,7,6,1,5};
+        List<List<Integer>> sums = combinationSum2(candidates, 8);
+        for(List<Integer> list : sums) {
+            for(int i : list)
+                System.out.print(i+" ");
+            System.out.println();
         }
     }
 }
